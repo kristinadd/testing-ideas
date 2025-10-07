41ee38cb3e300c173f7a39ac9833f08de02a682b
kristinadd <kristina@productboard.com>
2025-10-07 20:36:53 +0200

    Add Associations to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_07_162735) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_07_182303) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "draft"
+    t.datetime "published_at"
   end
+
+  add_foreign_key "comments", "posts"
 end
